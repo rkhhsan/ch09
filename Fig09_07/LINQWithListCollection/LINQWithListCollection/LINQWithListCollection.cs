9107f7d8390a6e9d0045b66f4258d0821fa5dbd6
Roberto Kai Hsi Hoo <prof.robertohoo@gmail.com>
2020-04-28 12:26:53 -0300

    ok

--- Fig09_07/LINQWithListCollection/LINQWithListCollection/LINQWithListCollection.cs
+++ Fig09_07/LINQWithListCollection/LINQWithListCollection/LINQWithListCollection.cs
@@ -6,37 +6,55 @@ using System.Collections.Generic;
 
 public class LINQWithListCollection
 {
-   public static void Main( string[] args )
+   public static void Main(string[] args)
    {
       // populate a List of strings 
+      /*
       List< string > items = new List< string >();
       items.Add( "aQua" ); // add "aQua" to the end of the List
       items.Add( "RusT" ); // add "RusT" to the end of the List
       items.Add( "yElLow" ); // add "yElLow" to the end of the List
       items.Add( "rEd" ); // add "rEd" to the end of the List
+      */
+      List<string> items = new List<string> { "aQua", "RusT", "yElLow", "rEd" };
 
       // convert all strings to uppercase; select those starting with "R"
       var startsWithR =
          from item in items
          let uppercaseString = item.ToUpper()
-         where uppercaseString.StartsWith( "R" )
+         where uppercaseString.StartsWith("R")
          orderby uppercaseString
          select uppercaseString;
 
       // display query results
-      foreach ( var item in startsWithR )
-         Console.Write( "{0} ", item );
+      foreach (var item in startsWithR)
+         Console.Write("{0} ", item);
 
       Console.WriteLine(); // output end of line
 
-      items.Add( "rUbY" ); // add "rUbY" to the end of the List
-      items.Add( "SaFfRon" ); // add "SaFfRon" to the end of the List
+      items.Add("rUbY"); // add "rUbY" to the end of the List
+      items.Add("SaFfRon"); // add "SaFfRon" to the end of the List
 
       // display updated query results
-      foreach ( var item in startsWithR )
-         Console.Write( "{0} ", item );
+      foreach (var item in startsWithR)
+         Console.Write("{0} ", item);
 
       Console.WriteLine(); // output end of line
+
+      // convert all strings to lowercase; select those starting with "R"
+      var lowercaseWords =
+         from item in items
+         let lowercaseString = item.ToLower()
+         orderby lowercaseString descending
+         select lowercaseString;
+
+      // display query results
+      foreach (var item in lowercaseWords)
+         Console.Write("{0} ", item);
+
+
+      Console.WriteLine("\nTecle qualquer tecla para finalizar...");
+      Console.ReadKey();
    } // end Main
 } // end class LINQWithListCollection
 
